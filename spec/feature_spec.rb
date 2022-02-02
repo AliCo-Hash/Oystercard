7382fd08f1466c922bb840e93c20d6b89b7e15ec
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-02-02 17:59:05 +0000

    User Story 10
    Co-authored-by: AliCo-Hash <AliCo-Hash@users.noreply.github.com>

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -7,7 +7,7 @@ describe "User Story 8" do
   it "stores the station on touch in" do
     oyster_card = Oystercard.new
     oyster_card.top_up(1)
-    station = Station.new("Liverpool Street")
+    station = Station.new("Liverpool Street", 1)
     oyster_card.touch_in(station)
     expect(oyster_card.entry_station.name).to eq "Liverpool Street"
   end
@@ -15,9 +15,9 @@ describe "User Story 8" do
   it 'set entry station to nil on touch out' do
     oyster_card = Oystercard.new
     oyster_card.top_up(1)
-    station = Station.new("Liverpool Street")
+    station = Station.new("Liverpool Street", 1)
     oyster_card.touch_in(station)
-    exit_station = Station.new("Holborn")
+    exit_station = Station.new("Holborn", 2)
     oyster_card.touch_out(exit_station)
     expect(oyster_card.entry_station).to be_nil
   end
@@ -31,9 +31,9 @@ describe "User Story 9" do
   it "store the station on touch out" do
     oyster_card = Oystercard.new
     oyster_card.top_up(1)
-    entry_station = Station.new("Liverpool Street")
+    entry_station = Station.new("Liverpool Street", 1)
     oyster_card.touch_in(entry_station)
-    exit_station = Station.new("Holborn")
+    exit_station = Station.new("Holborn", 2)
     oyster_card.touch_out(exit_station)
     expect(oyster_card.exit_station.name).to eq "Holborn"
   end
@@ -41,10 +41,20 @@ describe "User Story 9" do
   it 'stores the journey history' do
     oyster_card = Oystercard.new
     oyster_card.top_up(1)
-    entry_station = Station.new("Liverpool Street")
+    entry_station = Station.new("Liverpool Street", 1)
     oyster_card.touch_in(entry_station)
-    exit_station = Station.new("Holborn")
+    exit_station = Station.new("Holborn", 2)
     oyster_card.touch_out(exit_station)
     expect(oyster_card.journey_history).to include("Liverpool Street" => "Holborn")
+  end
+end
+
+# In order to know how far I have travelled
+# As a customer
+# I want to know what zone a station is in
+describe "User Story 10" do
+  xit "shows the zone" do
+    station = Station.new("Liverpool Street", 1)
+    expect(station.zone).to eq 1
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,10 +1,13 @@
 
 describe Station do 
   
-  subject(:station) { described_class.new("Liverpool Street") }
+  subject(:station) { described_class.new("Liverpool Street", 1) }
 
   it "has a name" do
     expect(station.name).to eq "Liverpool Street"
   end
 
+  it "shows the zone" do
+    expect(station.zone).to eq 1
+  end
 end
